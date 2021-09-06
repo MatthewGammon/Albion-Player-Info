3668f3bf06a66f09cd7f42b1fa7ce1a2edc796bf
Matthew Gammon <mgammon91@gmail.com>
2021-09-06 08:39:17 -0400

    modified app to contain all routes for the application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import './App.css';
-import Header from './layout/Header';
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Home from './home/Home';
+import Header from './layout/header/Header';
+import Footer from './layout/footer/Footer';
+import UserProfile from '../src/player/UserProfile';
+import NotFound from '../src/layout/notFound/NotFound';
+import PlayerId from './player/PlayerId';
+import PlayerSearch from './player/PlayerSearch';
+import PlayerDeaths from './player/PlayerDeaths';
 
-function App() {
+import './App.css';
+import '../src/layout/header/Header.css';
+import '../src/layout/footer/Footer.css';
+
+export default function App() {
   return (
-    <div>
+    <>
       <Header />
-      <Home />
-    </div>
+      <Footer />
+      <Switch>
+        <Route exact={true} path="/">
+          <Redirect to={'/home'} />
+        </Route>
+        <Route exact={true} path="/userProfile">
+          <UserProfile />
+        </Route>
+        <Route exact={true} path="/playerId">
+          <PlayerId />
+        </Route>
+        <Route path="/playerSearch">
+          <PlayerSearch />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/playerDeaths">
+          <PlayerDeaths />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </>
   );
 }
-
-export default App;
