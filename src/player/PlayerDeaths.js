892e45b74b4d7031a31cb566ed886051f61717ce
Matthew Gammon <mgammon91@gmail.com>
2021-09-06 10:17:47 -0400

    added columns to display Killer Avg IP, Victim's IP, and Deathfame. Added h1 that displays the user's name followed by 'Most Recent Deaths'

--- src/player/PlayerDeaths.js
+++ src/player/PlayerDeaths.js
@@ -4,7 +4,9 @@ import { useLocation } from 'react-router-dom';
 export default function PlayerDeaths() {
   const [deathData, setDeathData] = useState(null);
   const location = useLocation();
-  const playerId = location.state;
+  const playerId = location.state.playerId;
+  const name = location.state.name;
+  console.log(deathData);
 
   useEffect(loadData, [playerId]);
 
@@ -18,14 +20,23 @@ export default function PlayerDeaths() {
   if (deathData) {
     content = deathData.map((death, index) => (
       <div key={index} className="d-flex">
-        <div className="col-3">
+        <div className="col-2">
           <p>{death.Killer.Name}</p>
         </div>
-        <div className="col-3">
+        <div className="col-2">
           <p>{death.Killer.GuildName}</p>
         </div>
-        <div className="col-3">
-          <p>{death.numberOfParticipants}</p>
+        <div className="col-2">
+          <p>{death.Killer.AllianceName}</p>
+        </div>
+        <div className="col-2">
+          <p>{death.Victim.DeathFame}</p>
+        </div>
+        <div className="col-2">
+          <p>{Math.floor(death.Killer.AverageItemPower)}</p>
+        </div>
+        <div className="col-2">
+          <p>{Math.floor(death.Victim.AverageItemPower)}</p>
         </div>
       </div>
     ));
@@ -33,11 +44,19 @@ export default function PlayerDeaths() {
 
   return (
     <main className="d-flex flex-column ms-4">
-      <div className="headers row">
-        <h4 className="col-3">Killer Name</h4>
-        <h4 className="col-3">Killer's Guild</h4>
-        <h4 className="col-3">Number of Participants</h4>
+      <div className="text-center mb-3">
+        <h1>{name}'s Most Recent Deaths</h1>
       </div>
+
+      <div className="headers row">
+        <h4 className="col-2">Killed By</h4>
+        <h4 className="col-2">Guild</h4>
+        <h4 className="col-2">Alliance</h4>
+        <h4 className="col-2">Death Fame</h4>
+        <h4 className="col-2">Killer's Avg IP</h4>
+        <h4 className="col-2">Victim's IP</h4>
+      </div>
+      <hr />
       <div className="row">{content}</div>
     </main>
   );
