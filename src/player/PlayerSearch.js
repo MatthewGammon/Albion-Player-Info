fbcdbcf09fb769ad154d0d85c118fb0d7418fa8d
Matthew Gammon <mgammon91@gmail.com>
2021-09-06 10:19:46 -0400

    changed bg to dark and text to light. removed passing in userName as a prop to PlayerId

--- src/player/PlayerSearch.js
+++ src/player/PlayerSearch.js
@@ -26,13 +26,13 @@ export default function Home() {
 
   return (
     <main
-      className="form d-flex flex-column align-items-center  p-3"
+      className="form d-flex flex-column align-items-center  p-3 bg-dark text-light"
       style={{ backgroundColor: 'white' }}
     >
       <h1>Enter Your Character's Name</h1>
       <form onSubmit={submitHandler}>
         <label className="mt-4" htmlFor="name">
-          Name
+          Name:
         </label>
         <input
           className="m-2"
@@ -54,7 +54,7 @@ export default function Home() {
         API. Thank you.
       </h3>
 
-      <PlayerId playerInfo={playerInfo} userName={userName.name} />
+      <PlayerId playerInfo={playerInfo} />
     </main>
   );
 }
