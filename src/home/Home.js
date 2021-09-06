13d487eabe10eee26240b13ccb33526041ea1460
Matthew Gammon <mgammon91@gmail.com>
2021-09-06 09:20:58 -0400

    Changed some text for the landing page to reflect the current status of the project

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-export default function Home1() {
+export default function Home() {
   const history = useHistory();
   const handleClick = () => {
     history.push('/playerSearch');
   };
+
   return (
-    <>
-      <main
-        className="form d-flex flex-column align-items-center  p-3"
-        style={{ backgroundColor: 'white' }}
-      >
-        <h1>Look at me. I am the home page now.</h1>
-        <h2>Nothing else fucking works.</h2>
-        <button type="button" onClick={handleClick}>
-          Search For Your Character
-        </button>
-      </main>
-    </>
+    <div className="form d-flex flex-column align-items-center  p-3">
+      <div className="text-center">
+        <h2>Welcome to Conflict's proprietary regear system!</h2>
+        <p>This application is still a work in progress.</p>
+        <p>
+          Feel free to poke around. There is some base functionality for viewing
+          player data, but the regear system is not yet implemented.
+        </p>
+        <p>Styling will come after all functionality is in place.</p>
+      </div>
+      <button className="btn btn-primary" type="button" onClick={handleClick}>
+        Search For Your Character
+      </button>
+    </div>
   );
 }
