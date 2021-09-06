8db89d375c04966a5b3f2564c99a15682b2b8856
Matthew Gammon <mgammon91@gmail.com>
2021-09-06 08:47:34 -0400

    various files relocated, deleted, and renamed

--- src/player/PlayerId.js
+++ src/player/PlayerId.js
@@ -1,20 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import NotFound from '../layout/NotFound';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PlayerNotFound from './PlayerNotFound';
 
-export default function PlayerId({ playerInfo }) {
+export default function PlayerId({ userName, playerInfo }) {
   if (playerInfo && playerInfo.players.length !== 0) {
+    const playerId = playerInfo.players[0].Id;
+
     return (
-      <div className="playerInfo mt-3 text-center bg-dark text-light">
+      <div className="playerInfo mt-5 p-3 text-center bg-dark text-light">
         <h2>Success!</h2>
-        <h3>Your Albion Online player id is: {playerInfo.players[0].Id}</h3>
-        <a
-          href={`https://gameinfo.albiononline.com/api/gameinfo/players/${playerInfo.players[0].Id}`}
-        >
-          Click Me!
-        </a>
+        <h3>Your Albion Online player id is: {playerId}</h3>
+        <div>
+          <Link
+            to={{
+              pathname: `/userProfile`,
+              state: playerId,
+            }}
+          >
+            View My Profile
+          </Link>
+        </div>
       </div>
     );
   } else {
-    return <NotFound playerInfo={playerInfo} />;
+    return <PlayerNotFound playerInfo={playerInfo} />;
   }
 }
