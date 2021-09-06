3e33fe77361ef1e6358d2301881daeec386f7832
Matthew Gammon <mgammon91@gmail.com>
2021-09-06 08:47:09 -0400

    created a component that renders basic information about a player in card format using bootstrap cards

---
+++ src/player/UserProfile.js
@@ -0,0 +1,66 @@
+import { React, useEffect, useState } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import './UserProfile.css';
+
+export default function UserProfile() {
+  const [playerData, setPlayerData] = useState(null);
+  const location = useLocation();
+  const playerId = location.state;
+  console.log(playerData);
+
+  useEffect(loadData, [playerId]);
+
+  function loadData() {
+    fetch(`/players/${playerId}`)
+      .then((res) => res.json())
+      .then(setPlayerData);
+  }
+
+  return (
+    playerData && (
+      <main className="player-info d-flex justify-content-center">
+        <div className="card">
+          <img
+            src="https://i.imgur.com/4YKxVQj.png"
+            className="card-img-top"
+            alt="albion male character"
+          />
+          <div className="card-body">
+            <h3 className="card-title">Player: {playerData.Name}</h3>
+            <h5 className="card-text">Guild: {playerData.GuildName}</h5>
+            <h5 className="card-text">Alliance: {playerData.AllianceName}</h5>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              Kill Fame: {playerData.KillFame}
+            </li>
+            <li className="list-group-item">
+              Death Fame: {playerData.DeathFame}
+            </li>
+            <li className="list-group-item">
+              Fame Ratio: {playerData.FameRatio}
+            </li>
+          </ul>
+          <div className="card-body">
+            <Link
+              to={{
+                pathname: '/playerDeaths',
+                state: playerId,
+              }}
+            >
+              Recent Deaths
+            </Link>
+
+            <a
+              href="https://render.albiononline.com/v1/item/Master's%20Bloodletter.png?locale=en"
+              alt="bloodletter"
+              className="card-link"
+            >
+              Another link
+            </a>
+          </div>
+        </div>
+      </main>
+    )
+  );
+}
