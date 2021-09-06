b1105c045627fc37903a6568de87aa11911dc03b
Matthew Gammon <mgammon91@gmail.com>
2021-09-06 10:18:54 -0400

    removed userName from params as it is not being used. changed bg color to 'info' to stand out against the dark background

--- src/player/PlayerId.js
+++ src/player/PlayerId.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PlayerNotFound from './PlayerNotFound';
+import './PlayerId.css';
 
-export default function PlayerId({ userName, playerInfo }) {
+export default function PlayerId({ playerInfo }) {
   if (playerInfo && playerInfo.players.length !== 0) {
     const playerId = playerInfo.players[0].Id;
 
     return (
-      <div className="playerInfo mt-5 p-3 text-center bg-dark text-light">
+      <div className="playerInfo mt-5 p-3 text-center bg-info text-dark">
         <h2>Success!</h2>
         <h3>Your Albion Online player id is: {playerId}</h3>
         <div>
