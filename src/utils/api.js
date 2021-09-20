74fe4ea1b685284ef8cf82c2ba664b77d790dc46
Matthew Gammon <mgammon91@gmail.com>
2021-09-20 16:57:20 -0400

    changed timeout to 30s instead of 16 as the albion api response time varies greatly

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 export async function fetchWithTimeout(url, options = {}) {
-  const { timeout = 16000 } = options;
+  const { timeout = 30000 } = options;
 
   const abortController = new AbortController();
   const id = setTimeout(() => abortController.abort(), timeout);
