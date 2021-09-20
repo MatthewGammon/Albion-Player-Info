708f029ffa740c51cb03270e7dc222f2c7a01526
Matthew Gammon <mgammon91@gmail.com>
2021-09-20 00:39:47 -0400

    added state for loading. added spinner loading component so a user knows when the page is loading

--- src/player/PlayerDeaths.js
+++ src/player/PlayerDeaths.js
@@ -1,19 +1,23 @@
 import { React, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import Spinner from 'react-bootstrap/Spinner';
 
 export default function PlayerDeaths() {
   const [deathData, setDeathData] = useState(null);
+  const [isLoading, setIsLoading] = useState();
+
   const location = useLocation();
   const playerId = location.state.playerId;
   const name = location.state.name;
-  console.log(deathData);
 
   useEffect(loadData, [playerId]);
 
   function loadData() {
+    setIsLoading(true);
     fetch(`/players/${playerId}/deaths`)
       .then((res) => res.json())
-      .then(setDeathData);
+      .then(setDeathData)
+      .then(() => setIsLoading(false));
   }
 
   let content;
@@ -45,6 +49,16 @@ export default function PlayerDeaths() {
   return (
     <main className="d-flex flex-column ms-4">
       <div className="text-center mb-3">
+        {isLoading ? (
+          <div className="loading-deaths">
+            <h2>Loading</h2>
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </div>
+        ) : (
+          ''
+        )}
         <h1>{name}'s Most Recent Deaths</h1>
       </div>
 
