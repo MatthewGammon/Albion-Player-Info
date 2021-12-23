e18b57139eadcc942035684693a1fd2e613782d4
Matthew Gammon <mgammon91@gmail.com>
2021-12-22 21:56:35 -0500

    increased timeout by 15 seconds to allow for more time for the Albion API to reply. was timing out too frequently.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 export const baseUrl = process.env.REACT_APP_BASE_URL;
 
 export async function fetchWithTimeout(url, options = {}) {
-  const { timeout = 45000 } = options;
+  const { timeout = 60000 } = options;
 
   const abortController = new AbortController();
   const id = setTimeout(() => abortController.abort(), timeout);
