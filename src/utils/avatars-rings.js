1de254ce4d7a4cdcc20cfd3188e23736fe1fc1fb
Matthew Gammon <mgammon91@gmail.com>
2021-12-30 21:02:54 -0500

    deleted old avatar and ring imports and added 65 avatars and 31 rings

--- src/utils/avatars-rings.js
+++ src/utils/avatars-rings.js
@@ -1,24 +1,169 @@
 // importing images for use as player avatars
-import imageOne from '../images/avatars/0tGrbNG.png';
-import imageTwo from '../images/avatars/4kc4Aku.png';
-import imageThree from '../images/avatars/905I2oh.png';
-import imageFour from '../images/avatars/i2eBBIw.png';
-import imageFive from '../images/avatars/RfKqwVO.png';
-import imageSix from '../images/avatars/rvACBed.png';
-import imageSeven from '../images/avatars/yKQyEkt.png';
+import avatar1 from '../assets/images/avatars/avatar-01.png';
+import avatar2 from '../assets/images/avatars/avatar-02.png';
+import avatar3 from '../assets/images/avatars/avatar-03.png';
+import avatar4 from '../assets/images/avatars/avatar-04.png';
+import avatar5 from '../assets/images/avatars/avatar-05.png';
+import avatar6 from '../assets/images/avatars/avatar-06.png';
+import avatar7 from '../assets/images/avatars/avatar-07.png';
+import avatar8 from '../assets/images/avatars/avatar-08.png';
+import avatar9 from '../assets/images/avatars/avatar-09.png';
+import avatar10 from '../assets/images/avatars/avatar-10.png';
+import avatar11 from '../assets/images/avatars/avatar-11.png';
+import avatar12 from '../assets/images/avatars/avatar-12.png';
+import avatar13 from '../assets/images/avatars/avatar-13.png';
+import avatar14 from '../assets/images/avatars/avatar-14.png';
+import avatar15 from '../assets/images/avatars/avatar-15.png';
+import avatar16 from '../assets/images/avatars/avatar-16.png';
+import avatar17 from '../assets/images/avatars/avatar-17.png';
+import avatar18 from '../assets/images/avatars/avatar-18.png';
+import avatar19 from '../assets/images/avatars/avatar-19.png';
+import avatar20 from '../assets/images/avatars/avatar-20.png';
+import avatar21 from '../assets/images/avatars/avatar-21.png';
+import avatar22 from '../assets/images/avatars/avatar-22.png';
+import avatar23 from '../assets/images/avatars/avatar-23.png';
+import avatar24 from '../assets/images/avatars/avatar-24.png';
+import avatar25 from '../assets/images/avatars/avatar-25.png';
+import avatar26 from '../assets/images/avatars/avatar-26.png';
+import avatar27 from '../assets/images/avatars/avatar-27.png';
+import avatar28 from '../assets/images/avatars/avatar-28.png';
+import avatar29 from '../assets/images/avatars/avatar-29.png';
+import avatar30 from '../assets/images/avatars/avatar-30.png';
+import avatar31 from '../assets/images/avatars/avatar-31.png';
+import avatar32 from '../assets/images/avatars/avatar-32.png';
+import avatar33 from '../assets/images/avatars/avatar-33.png';
+import avatar34 from '../assets/images/avatars/avatar-34.png';
+import avatar35 from '../assets/images/avatars/avatar-35.png';
+import avatar36 from '../assets/images/avatars/avatar-36.png';
+import avatar37 from '../assets/images/avatars/avatar-37.png';
+import avatar38 from '../assets/images/avatars/avatar-38.png';
+import avatar39 from '../assets/images/avatars/avatar-39.png';
+import avatar40 from '../assets/images/avatars/avatar-40.png';
+import avatar41 from '../assets/images/avatars/avatar-41.png';
+import avatar42 from '../assets/images/avatars/avatar-42.png';
+import avatar43 from '../assets/images/avatars/avatar-43.png';
+import avatar44 from '../assets/images/avatars/avatar-44.png';
+import avatar45 from '../assets/images/avatars/avatar-45.png';
+import avatar46 from '../assets/images/avatars/avatar-46.png';
+import avatar47 from '../assets/images/avatars/avatar-47.png';
+import avatar48 from '../assets/images/avatars/avatar-48.png';
+import avatar49 from '../assets/images/avatars/avatar-49.png';
+import avatar50 from '../assets/images/avatars/avatar-50.png';
+import avatar51 from '../assets/images/avatars/avatar-51.png';
+import avatar52 from '../assets/images/avatars/avatar-52.png';
+import avatar53 from '../assets/images/avatars/avatar-53.png';
+import avatar54 from '../assets/images/avatars/avatar-54.png';
+import avatar55 from '../assets/images/avatars/avatar-55.png';
+import avatar56 from '../assets/images/avatars/avatar-56.png';
+import avatar57 from '../assets/images/avatars/avatar-57.png';
+import avatar58 from '../assets/images/avatars/avatar-58.png';
+import avatar59 from '../assets/images/avatars/avatar-59.png';
+import avatar60 from '../assets/images/avatars/avatar-60.png';
+import avatar61 from '../assets/images/avatars/avatar-61.png';
+import avatar62 from '../assets/images/avatars/avatar-62.png';
+import avatar63 from '../assets/images/avatars/avatar-63.png';
+import avatar64 from '../assets/images/avatars/avatar-64.png';
+import avatar65 from '../assets/images/avatars/avatar-65.png';
+// importing images for use as avatar rings
 
-// importing images for use as avatar rigns
-import ringOne from '../images/rings/orwO9G0.png';
-import ringTwo from '../images/rings/orLtClP.png';
+import ring1 from '../assets/images/rings/ring-01.png';
+import ring2 from '../assets/images/rings/ring-02.png';
+import ring3 from '../assets/images/rings/ring-03.png';
+import ring4 from '../assets/images/rings/ring-04.png';
+import ring5 from '../assets/images/rings/ring-05.png';
+import ring6 from '../assets/images/rings/ring-06.png';
+import ring7 from '../assets/images/rings/ring-07.png';
+import ring8 from '../assets/images/rings/ring-08.png';
+import ring9 from '../assets/images/rings/ring-09.png';
+import ring10 from '../assets/images/rings/ring-10.png';
+import ring11 from '../assets/images/rings/ring-11.png';
+import ring12 from '../assets/images/rings/ring-12.png';
+import ring13 from '../assets/images/rings/ring-13.png';
+import ring14 from '../assets/images/rings/ring-14.png';
+import ring15 from '../assets/images/rings/ring-15.png';
+import ring16 from '../assets/images/rings/ring-16.png';
+import ring17 from '../assets/images/rings/ring-17.png';
+import ring18 from '../assets/images/rings/ring-18.png';
+import ring19 from '../assets/images/rings/ring-19.png';
+import ring20 from '../assets/images/rings/ring-20.png';
+import ring21 from '../assets/images/rings/ring-21.png';
+import ring22 from '../assets/images/rings/ring-22.png';
+import ring23 from '../assets/images/rings/ring-23.png';
+import ring24 from '../assets/images/rings/ring-24.png';
+import ring25 from '../assets/images/rings/ring-25.png';
+import ring26 from '../assets/images/rings/ring-26.png';
+import ring27 from '../assets/images/rings/ring-27.png';
+import ring28 from '../assets/images/rings/ring-28.png';
+import ring29 from '../assets/images/rings/ring-29.png';
+import ring30 from '../assets/images/rings/ring-30.png';
+import ring31 from '../assets/images/rings/ring-31.png';
 // select and export random avatar for use in UserProfile
 const avatars = [
-  imageOne,
-  imageTwo,
-  imageThree,
-  imageFour,
-  imageFive,
-  imageSix,
-  imageSeven,
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  avatar5,
+  avatar6,
+  avatar7,
+  avatar8,
+  avatar9,
+  avatar10,
+  avatar11,
+  avatar12,
+  avatar13,
+  avatar14,
+  avatar15,
+  avatar16,
+  avatar17,
+  avatar18,
+  avatar19,
+  avatar20,
+  avatar21,
+  avatar22,
+  avatar23,
+  avatar24,
+  avatar25,
+  avatar26,
+  avatar27,
+  avatar28,
+  avatar29,
+  avatar30,
+  avatar31,
+  avatar32,
+  avatar33,
+  avatar34,
+  avatar35,
+  avatar36,
+  avatar37,
+  avatar38,
+  avatar39,
+  avatar40,
+  avatar41,
+  avatar42,
+  avatar43,
+  avatar44,
+  avatar45,
+  avatar46,
+  avatar47,
+  avatar48,
+  avatar49,
+  avatar50,
+  avatar51,
+  avatar52,
+  avatar53,
+  avatar54,
+  avatar55,
+  avatar56,
+  avatar57,
+  avatar58,
+  avatar59,
+  avatar60,
+  avatar61,
+  avatar62,
+  avatar63,
+  avatar64,
+  avatar65,
 ];
 
 // having two values for a random index is redundant. I should create a function that takes in an array and returns an index and use that function to get my avatar and ring
@@ -27,6 +172,38 @@ const avatarIndex = Math.floor(Math.random() * avatars.length);
 export const avatar = avatars[avatarIndex];
 
 // select and export random ring for use in UserProfile
-const rings = [ringOne, ringTwo];
+const rings = [
+  ring1,
+  ring2,
+  ring3,
+  ring4,
+  ring5,
+  ring6,
+  ring7,
+  ring8,
+  ring9,
+  ring10,
+  ring11,
+  ring12,
+  ring13,
+  ring14,
+  ring15,
+  ring16,
+  ring17,
+  ring18,
+  ring19,
+  ring20,
+  ring21,
+  ring22,
+  ring23,
+  ring24,
+  ring25,
+  ring26,
+  ring27,
+  ring28,
+  ring29,
+  ring30,
+  ring31,
+];
 const ringIndex = Math.floor(Math.random() * rings.length);
 export const ring = rings[ringIndex];
