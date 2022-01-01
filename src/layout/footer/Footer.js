02277a30bfdafbd866e669d3dd30e2b49827ba47
Matthew Gammon <mgammon91@gmail.com>
2021-12-31 23:05:48 -0500

    changed file structure. all component directories are now contained in the 'components' directory

--- src/layout/footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <p className="footer text-light text-center">
-      Copyright 2021 | Matthew Gammon
-    </p>
-  );
-}
