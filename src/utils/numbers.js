abbf3170b3294d30d27cec4e0ec2094c7d0ff72c
Matthew Gammon <mgammon91@gmail.com>
2022-01-05 14:56:54 -0500

    created a generateIndex func which takes in an array and returns a random index. Changed exports.

--- src/utils/numbers.js
+++ src/utils/numbers.js
@@ -1,5 +1,14 @@
-export default function separator(numb) {
+function separator(numb) {
   let str = numb.toString().split('.');
   str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   return str.join('.');
 }
+
+function generateIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+module.exports = {
+  separator,
+  generateIndex,
+};
