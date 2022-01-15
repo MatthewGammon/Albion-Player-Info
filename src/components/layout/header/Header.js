9ad42585e228d0d69ab31cea4d41f41b7e275a01
Matthew Gammon <mgammon91@gmail.com>
2022-01-14 21:55:45 -0500

    restructured markup and changed classnames

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -4,21 +4,24 @@ import './Header.css';
 
 function Header() {
   return (
-    <div className="header">
-      <h1 className="main-text">Albion Online Player Info and Regear</h1>
-      <p className="lead">See your stats and submit regear requests!</p>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <div>
-            <Link to="/home">Home</Link>
-          </div>
-          <div className="ms-2">
-            <Link to="/playerSearch">Search</Link>
-          </div>
-          <div className="collapse navbar-collapse" id="navbarNav"></div>
-        </div>
+    <>
+      {' '}
+      <div className="header">
+        <h1 className="header-text">Albion Online Player Info and Regear</h1>
+        <p className="header-lead">
+          See your stats and submit regear requests!
+        </p>
+      </div>
+      <nav className="navbar">
+        <Link className="nav-link" to="/home">
+          Home
+        </Link>
+
+        <Link className="nav-link" to="/playerSearch">
+          Search
+        </Link>
       </nav>
-    </div>
+    </>
   );
 }
 
