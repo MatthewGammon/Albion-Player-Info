30237fee6ccb73cce9fd73b7cf5fcc8edd681a12
Matthew Gammon <mgammon91@gmail.com>
2022-01-31 17:27:06 -0500

    created func that takes in a character name and returns the corresponding playerId.

--- src/utils/api.js
+++ src/utils/api.js
@@ -13,3 +13,16 @@ export async function fetchWithTimeout(url, options = {}) {
   clearTimeout(id);
   return response;
 }
+
+export async function getPlayerId(characterName) {
+  try {
+    const response = await fetchWithTimeout(
+      `${baseUrl}/search?q=${characterName}`
+    );
+    const playerInfo = await response.json();
+    const playerId = playerInfo.players[0].Id;
+    return playerId;
+  } catch (error) {
+    console.error(error);
+  }
+}
