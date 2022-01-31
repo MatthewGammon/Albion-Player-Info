45d3980bd41b1a661127b4001baa9f0d7c56910e
Matthew Gammon <mgammon91@gmail.com>
2022-01-31 17:27:46 -0500

    changed style of exporting function to align with the rest of the project

--- src/components/layout/errors/ErrorAlert.js
+++ src/components/layout/errors/ErrorAlert.js
@@ -1,14 +1,4 @@
-import React from 'react';
-
-/**
- * Defines the alert message to render if the specified error is truthy.
- * @param error
- *  an instance of an object with `.message` property as a string, typically an Error instance.
- * @returns {JSX.Element}
- *  a bootstrap danger alert that contains the message string.
- */
-
-function ErrorAlert({ error }) {
+export default function ErrorAlert({ error }) {
   return (
     error && (
       <div className="alert alert-danger m-2 text-center">
@@ -17,5 +7,3 @@ function ErrorAlert({ error }) {
     )
   );
 }
-
-export default ErrorAlert;
