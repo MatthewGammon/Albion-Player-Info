45a719b42b27c076977e7901dfb616914327e3c2
Matthew Gammon <mgammon91@gmail.com>
2022-02-04 07:03:29 -0500

    removed the old nav bar and implemented a new, more mobile friendly nav. Added link to About section (currently does  not exist).

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,29 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 import CharacterSelect from '../characterSelect/CharacterSelect';
 import './Header.css';
 
 function Header() {
+  function displayMenu() {
+    const element = document.getElementById('myTopnav');
+
+    if (element.className === 'topnav') {
+      element.className += ' responsive';
+    } else {
+      element.className = 'topnav';
+    }
+  }
+
   return (
     <>
-      {' '}
+      <div className="topnav" id="myTopnav">
+        <a href="/" className="active">
+          Home
+        </a>
+        <a href="/search">Search</a>
+        <a href="/about">About</a>
+        <div className="character-select">
+          <CharacterSelect />
+        </div>
+
+        <a className="icon" onClick={() => displayMenu()}>
+          <i className="fa fa-bars"></i>
+        </a>
+      </div>
+
       <div className="header">
         <h1 className="header-text">Albion Online Player Info and Regear</h1>
         <p className="header-lead">
           See your stats and submit regear requests!
         </p>
       </div>
-      <nav className="navbar">
-        <div className="navbar-links">
-          <Link className="nav-link" to="/home">
-            Home
-          </Link>
-          <Link className="nav-link" to="/playerSearch">
-            Search
-          </Link>
-        </div>
-        <CharacterSelect />
-      </nav>
     </>
   );
 }
