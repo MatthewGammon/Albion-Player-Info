aaaef0f895c1a51de390c1c6c8f6baf0e0228f12
Matthew Gammon <mgammon91@gmail.com>
2022-02-18 20:28:39 -0500

    create regearBaseUrl, headers object, fetchJson func, and createRegearSubmission func

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,11 @@
 export const baseUrl = process.env.REACT_APP_BASE_URL;
 
+const regearBaseUrl =
+  process.env.REACT_APP_REGEAR_BASE_URL || 'http://localhost:5000';
+
+const headers = new Headers();
+headers.append('Content-Type', 'application/json');
+
 export async function fetchWithTimeout(url, options = {}) {
   const { timeout = 30000 } = options;
 
@@ -25,4 +31,38 @@ export async function getPlayerId(characterName) {
   } catch (error) {
     console.error(error);
   }
+}
+
+async function fetchJson(url, options, onCancel) {
+  try {
+    const response = await fetch(url, options);
+
+    if (response.status === 204) {
+      return null;
+    }
+
+    const payload = await response.json();
+
+    if (payload.error) {
+      return Promise.reject({ message: payload.error });
+    }
+    return payload.data;
+  } catch (error) {
+    if (error.name !== 'AbortError') {
+      console.error(error.stack);
+      throw error;
+    }
+    return Promise.resolve(onCancel);
+  }
+}
+
+export async function createRegearSubmission(submission, signal) {
+  const url = `${regearBaseUrl}/regears`;
+  const options = {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({ data: submission }),
+    signal,
+  };
+  return await fetchJson(url, options);
 }
