a3db29baf8aa3c002117a56ac3bf34f4a1e9bcaf
Matthew Gammon <mgammon91@gmail.com>
2022-03-01 01:07:16 -0500

    remove bootstrap styling. change h2 to h4

--- src/components/layout/notFound/PlayerNotFound.js
+++ src/components/layout/notFound/PlayerNotFound.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function NotFound({ playerInfo }) {
   if (playerInfo && playerInfo.players.length === 0) {
     return (
-      <div className="text-center bg-dark text-warning mt-4 p-2 rounded">
-        <h2 className="m-2 p-1">Character not found!</h2>
+      <div className="m-1">
+        <h4>Character not found!</h4>
         <p>Please check your spelling and try again.</p>
       </div>
     );
