945ae07405e6ecca74373ef4ef89c69097e9fde2
Matthew Gammon <mgammon91@gmail.com>
2022-06-22 14:26:09 -0400

    fix incorrect url

--- src/utils/api.js
+++ src/utils/api.js
@@ -56,7 +56,7 @@ async function fetchJson(url, options, onCancel) {
 }
 
 export async function createRegearSubmission(submission, signal) {
-  const url = `${regearBaseUrl}/regears/request`;
+  const url = `${regearBaseUrl}/generateRegearRequest`;
   const options = {
     method: 'POST',
     headers,
