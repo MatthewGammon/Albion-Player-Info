28c91ae393a705de7b4a27c7b5696136a8be47ac
Matthew Gammon <mgammon91@gmail.com>
2022-06-22 15:42:36 -0400

    revert createRegearSubmission url back to /regears

--- src/utils/api.js
+++ src/utils/api.js
@@ -56,7 +56,7 @@ async function fetchJson(url, options, onCancel) {
 }
 
 export async function createRegearSubmission(submission, signal) {
-  const url = `${regearBaseUrl}/generateRegearRequest`;
+  const url = `${regearBaseUrl}/regears`;
   const options = {
     method: 'POST',
     headers,
